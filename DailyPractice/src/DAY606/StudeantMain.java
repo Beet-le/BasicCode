d498ee68e737e06d38c4665f1794b69478e38179
Bee-le <1693332907@qq.com>
2021-06-06 18:48:28 +0800

    每日练习

---
+++ DailyPractice/src/DAY606/StudeantMain.java
@@ -0,0 +1,10 @@
+package DAY606;
+
+public class StudeantMain {
+    public static void main(String[] args) {
+        Student stu=new Student();
+        stu.method();
+        stu.methodStatic();
+        Student.methodStatic();
+    }
+}
---
+++ DailyPractice/src/DAY606/Student.java
@@ -0,0 +1,10 @@
+package DAY606;
+
+public class Student {
+    public void method(){
+        System.out.println("这是一个普通的成员方法方法");
+    }
+    public static void methodStatic(){
+        System.out.println("这是一个静态方法 ");
+    }
+}
