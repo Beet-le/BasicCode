f091878989efcfe774dd6dae3e1b7170ce40066f
Bee-le <1693332907@qq.com>
2021-06-15 18:27:11 +0800

    每日练习Interface

--- DailyPractice/src/Day615itcast/MyInterFace.java
+++ DailyPractice/src/Day615itcast/MyInterFace.java
@@ -2,5 +2,7 @@ package Day615itcast;
 
 public interface MyInterFace {
     //这是一个抽象方法   public abstract可以省略
-    public abstract void methodabs();
+    public abstract void methodabs1();
+    //这也是抽象方法
+    abstract void methodabs2();
 }
