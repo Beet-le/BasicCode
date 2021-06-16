e0a70c2e1404d0f4ced365cd8c5a59f7d000b561
Bee-le <1693332907@qq.com>
2021-06-16 22:09:29 +0800

    每日练习Interface

--- DailyPractice/src/Day616Interfacedefact/MyinterfacedefaultA.java
+++ DailyPractice/src/Day616Interfacedefact/MyinterfacedefaultA.java
@@ -1,4 +1,9 @@
 package Day616Interfacedefact;
 
-public class MyinterfacedefaultA {
+public class MyinterfacedefaultA implements MyinterfaceDefault{
+
+    @Override
+    public void methodABS() {
+        System.out.println("实现了抽象方法");
+    }
 }
