2c6a8e438da99ad193ba76650b8cf736b9096a7c
Bee-le <1693332907@qq.com>
2021-06-16 22:44:52 +0800

    每日练习Interface

--- DailyPractice/src/Day616Interfacedefact/DemoInterFace.java
+++ DailyPractice/src/Day616Interfacedefact/DemoInterFace.java
@@ -2,9 +2,10 @@ package Day616Interfacedefact;
 
 public class DemoInterFace {
     public static void main(String[] args) {
-        MyinterfacedefaultA a=new MyinterfacedefaultA();
-        MyinterfacedefaultB b=new MyinterfacedefaultB();
+        MyinterfacedefaultA a = new MyinterfacedefaultA();
+        MyinterfacedefaultB b = new MyinterfacedefaultB();
         a.methodABS();
         b.methodABS();
+        a.methodDefault();
     }
 }
--- DailyPractice/src/Day616Interfacedefact/MyinterfaceDefault.java
+++ DailyPractice/src/Day616Interfacedefact/MyinterfaceDefault.java
@@ -3,8 +3,10 @@ package Day616Interfacedefact;
 public interface MyinterfaceDefault {
     //這是一个抽象方法
     public abstract void methodABS();
-//    public abstract void methodABS2();
-    public default void methodDefault(){
 
+//    public abstract void methodABS2();
+
+    public default void methodDefault() {
+        System.out.println("新添加的默认方法");
     }
 }
