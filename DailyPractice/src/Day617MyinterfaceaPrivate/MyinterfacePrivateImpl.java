febe78de21be618a98ee9dbac38be1361e5f714a
Bee-le <1693332907@qq.com>
2021-06-17 21:53:26 +0800

    每日练习InterfaceStatic

---
+++ DailyPractice/src/Day617MyinterfaceaPrivate/MyinterfacePrivateImpl.java
@@ -0,0 +1,8 @@
+package Day617MyinterfaceaPrivate;
+
+public class MyinterfacePrivateImpl implements MyinterfacePrivate{
+    public void methodAnother(){
+        //直接访问到了接口中的默认方法，methodCommon错误的
+        methodCommon();
+    }
+}
