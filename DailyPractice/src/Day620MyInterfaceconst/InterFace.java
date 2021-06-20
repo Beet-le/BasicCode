83b2f5db7fe60cf96ab7725fb3e4ae1c2b625b0e
Bee-le <1693332907@qq.com>
2021-06-20 14:57:18 +0800

    每日练习InterfaceStatic

--- DailyPractice/src/Day620MyInterfaceconst/InterFace.java
+++ DailyPractice/src/Day620MyInterfaceconst/InterFace.java
@@ -2,6 +2,6 @@ package Day620MyInterfaceconst;
 
 public class InterFace {
     public static void main(String[] args) {
-        System.out.println(MyInterfaceConst.NUM);
+        System.out.println(MyInterfaceConst.NUM_OF_MY_STUDY);
     }
 }
--- DailyPractice/src/Day620MyInterfaceconst/MyInterfaceConst.java
+++ DailyPractice/src/Day620MyInterfaceconst/MyInterfaceConst.java
@@ -1,5 +1,5 @@
 package Day620MyInterfaceconst;
 //public static final 数据类型 常量名称=数据值；
 public interface MyInterfaceConst {
-    public static final int NUM =10;
+    public static final int NUM_OF_MY_STUDY =10;
 }
